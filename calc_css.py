bdeb342f5f28049905027b6e02ef78547494bea3
SAMAN IBRAHIM U <Ibrahimsaman157@gmail.com>
2026-07-24 22:31:42 +0530

    Update calc_css.py

--- calc_css.py
+++ calc_css.py
@@ -56,7 +56,6 @@ div[data-testid="stVerticalBlock"]{
 """, unsafe_allow_html=True)
 st.markdown("<h1 class='main-title'>💻 Trendy Calculator</h1>", unsafe_allow_html=True)
 st.markdown("<p class='subtitle'>Simple • Fast • Beautiful</p>", unsafe_allow_html=True)
-# ---------- Inputs ----------
 col1, col2 = st.columns(2)
 with col1:
     num1 = st.number_input("🔢 First Number", value=0.0)
